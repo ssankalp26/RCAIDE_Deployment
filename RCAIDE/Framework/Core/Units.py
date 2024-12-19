973570655475272979e48c25b80fa91687a9db5c
niranjann-14 <nn18@illinois.edu>
2024-12-19 16:08:33 -0600

    changes

--- RCAIDE/Framework/Core/Units.py
+++ RCAIDE/Framework/Core/Units.py
@@ -354,6 +354,10 @@ class Units():
     
     @classmethod
     @property
+    def lbs(cls): return cls(cls._POUND_TO_KG)
+    
+    @classmethod
+    @property
     def ounce(cls): return cls(cls._OUNCE_TO_KG)
     
     @classmethod
