2c08d72ac4785a611071976ede8fce7433ef37ad
niranjann-14 <nn18@illinois.edu>
2024-12-19 16:21:47 -0600

    change

--- RCAIDE/Framework/Core/Units.py
+++ RCAIDE/Framework/Core/Units.py
@@ -9,370 +9,246 @@
 #  Container
 # ----------------------------------------------------------------------------------------------------------------------   
 
-class Units():
-    """ RCAIDE.Core.Units()
-        Unit conversion toolbox
-        Works by converting values in to and out of the base unit
-        
-        Important Note and Warning - 
-            This does not enforce unit consistency!!!
-            Unit consistency is the responsibility of the user
-        
-        Usage:
-          from RCAIDE.Framework.Core import Units
-          a = 4. * Units.mm  # convert in to base unit
-          b = a  / Units.mm  # convert out of base unit
-          
-        Comments:
-          Retreving an attribute of Units (ie Units.mm) returns 
-          the conversion ratio to the base unit.  So in the above
-          example Units.mm = 0.001, which is the conversion ratio
-          to meters.  Thus the * (multiplication) operation converts 
-          from the current units to the base units and / (division) 
-          operation converts from the base units to the desired units.
-         
-        Base Units:
-          mass        : kilogram
-          length      : meters
-          time        : seconds
-          temperature : Kelvin
-          angle       : radian
-          current     : Ampere
-          luminsoity  : candela
+class Units:
+    """ 
+    RCAIDE.Core.Units()
+    A unit conversion toolbox that works by converting values into and out of the base units.
+
+    Important:
+        This does not enforce unit consistency. The user is responsible for ensuring that the 
+        units being converted are consistent and meaningful.
+
+    Usage:
+        from Units import Units
+        a = 4. * Units.mm   # Convert 4 millimeters into meters (the base length unit)
+        b = a / Units.mm    # Convert back from meters to millimeters
+
+    Explanation:
+        Accessing an attribute of Units (e.g. Units.mm) returns the conversion factor
+        to the base unit. For instance, Units.mm = 0.001 means 1 mm = 0.001 m.
+        Multiplying by Units.mm converts from millimeters to meters.
+        Dividing by Units.mm converts from meters to millimeters.
+    
+    Base Units:
+        mass        : kilogram (kg)
+        length      : meter (m)
+        time        : second (s)
+        temperature : Kelvin (K)
+        angle       : radian (rad)
+        current     : Ampere (A)
+        luminous intensity: candela (cd)
+        power       : watt (W)
+        pressure    : pascal (Pa)
+        area        : square meter (m²)
+        volume      : cubic meter (m³)
+
+    Note on Temperature:
+        Temperature scales involving offsets (°C, °F) cannot be handled by a simple scale factor.
+        If using this class for temperature, restrict yourself to conversions that are pure scale
+        factors (e.g., Rankine to Kelvin). Otherwise, handle offsets outside of this tool.
     """
-    # FUNDAMENTAL CONVERSION FACTORS
-    # Length
-    _FEET_TO_M = 0.3048
-    _INCH_TO_M = 0.0254
-    _MILE_TO_M = 1609.34
-    _NM_TO_M = 1852
-    _YARD_TO_M = 0.9144
-    _KM_TO_M = 1000
-    _DM_TO_M = 0.1
-    _CM_TO_M = 0.01
-    _MM_TO_M = 0.001
 
-    # Time
-    _MIN_TO_S = 60
-    _HOUR_TO_S = 3600
-    _DAY_TO_S = 86400
-    _WEEK_TO_S = 604800
-    _MONTH_TO_S = 2592000  # 30-day month
-    _YEAR_TO_S = 31536000  # 365-day year
-    _MS_TO_S = 0.001
+    # Fundamental conversion factors to base units:
+    # Length (base unit: meter)
+    length_conversions = {
+        'm': 1.0,
+        'km': 1000.0,
+        'dm': 0.1,
+        'cm': 0.01,
+        'mm': 0.001,
+        'micrometer': 1e-6,
+        'nanometer': 1e-9,
+        'inch': 0.0254,
+        'ft': 0.3048,
+        'yard': 0.9144,
+        'mile': 1609.34,
+        'nautical_mile': 1852.0
+    }
 
-    # Mass
-    _GRAM_TO_KG = 0.001
-    _MG_TO_KG = 1e-6
-    _POUND_TO_KG = 0.453592
-    _OUNCE_TO_KG = 0.0283495
-    _TON_TO_KG = 1000
+    # Time (base unit: second)
+    time_conversions = {
+        's': 1.0,
+        'ms': 0.001,
+        'minute': 60.0,
+        'hour': 3600.0,
+        'day': 86400.0,
+        'week': 604800.0,
+        'month': 2592000.0,  # 30-day month assumption
+        'year': 31536000.0   # 365-day year
+    }
 
-    # Force
-    _LBF_TO_N = 4.44822  # Pound force to Newton
+    # Mass (base unit: kilogram)
+    mass_conversions = {
+        'kg': 1.0,
+        'g': 0.001,
+        'mg': 1e-6,
+        'metric_ton': 1000.0,
+        'pound': 0.453592,
+        'lb': 0.453592,
+        'lbs': 0.453592,
+        'ounce': 0.0283495
+    }
 
-    # DERIVED CONVERSION FACTORS
-    # Area (using square of length factors)
-    _ACRE_TO_SQM = 4046.86
-    _SQYARD_TO_SQM = _YARD_TO_M ** 2
-    _SQFEET_TO_SQM = _FEET_TO_M ** 2
-    _SQINCH_TO_SQM = _INCH_TO_M ** 2
-    _SQKM_TO_SQM = _KM_TO_M ** 2
-    _SQDM_TO_SQM = _DM_TO_M ** 2
-    _SQCM_TO_SQM = _CM_TO_M ** 2
-    _HECTARE_TO_SQM = 10000
+    # Angle (base unit: radian)
+    angle_conversions = {
+        'radian': 1.0,
+        'degree': 0.0174533
+    }
 
-    # Volume (using cube of length factors)
-    _CUYD_TO_M3 = _YARD_TO_M ** 3
-    _CUFT_TO_M3 = _FEET_TO_M ** 3
-    _CUIN_TO_M3 = _INCH_TO_M ** 3
-    _LITER_TO_M3 = 0.001
-    _DL_TO_M3 = _LITER_TO_M3 / 10
-    _CL_TO_M3 = _LITER_TO_M3 / 100
-    _ML_TO_M3 = _LITER_TO_M3 / 1000
-    _GALLON_TO_M3 = 0.00378541
-    _QUART_TO_M3 = _GALLON_TO_M3 / 4
-    _PINT_TO_M3 = _GALLON_TO_M3 / 8
-    _FLOZ_TO_M3 = _GALLON_TO_M3 / 128
-    _BUSHEL_TO_M3 = 0.0352391
-    _BARREL_TO_M3 = 0.158987
+    # Current (base unit: Ampere)
+    current_conversions = {
+        'A': 1.0,
+        'mA': 1e-3,
+        'µA': 1e-6
+    }
 
-    # Power
-    _KW_TO_W = 1000
-    _BTU_PER_HOUR_TO_W = 0.29307107
-    _HP_TO_W = 745.7
+    # Luminous intensity (base unit: candela)
+    # Note: Conversions for luminous intensity are not as straightforward since candela is a base SI unit.
+    # Commonly, people convert luminous flux (lumens) and illuminance (lux), but these are different quantities.
+    # Here, we only provide direct scale multiples (e.g., millicandela).
+    luminous_intensity_conversions = {
+        'cd': 1.0,
+        'mcd': 1e-3
+    }
 
-    # Pressure (Force per unit area)
-    _ATM_TO_PA = 101325
-    _BAR_TO_PA = 100000
-    _MBAR_TO_PA = 100
-    _MPA_TO_PA = 1e6
-    _KPA_TO_PA = 1000
-    _PSI_TO_PA = _LBF_TO_N / (_INCH_TO_M ** 2)
-    _PSF_TO_PA = _LBF_TO_N / (_FEET_TO_M ** 2)
+    # Temperature (base unit: Kelvin)
+    # For temperature, we provide only ratio-based units. Realistically, Celsius and Fahrenheit require offsets.
+    # If strictly using ratio-based conversions, you could do something like Rankine = Kelvin * 1.8
+    temperature_conversions = {
+        'K': 1.0,
+        'R': 1.8,  # Rankine = Kelvin * 1.8 (no offset at absolute scale)
+        # Celsius and Fahrenheit are not included due to their offset nature.
+    }
 
-    # Speed (Distance per time)
-    _KNOT_TO_MS = _NM_TO_M / _HOUR_TO_S
-    _KMS_TO_MS = _KM_TO_M
-    _MPH_TO_MS = _MILE_TO_M / _HOUR_TO_S
-    _FPM_TO_MS = _FEET_TO_M / _MIN_TO_S
-    _FPS_TO_MS = _FEET_TO_M
+    # Area (base unit: square meter)
+    # Area conversions are derived from length conversions squared.
+    area_conversions = {
+        'square_meter': 1.0,
+        'hectare': 10000.0,
+        'acre': 4046.86,
+        'square_kilometer': 1000.0**2,
+        'square_decimeter': 0.1**2,
+        'square_centimeter': 0.01**2,
+        'square_millimeter': 0.001**2,
+        'square_inch': 0.0254**2,
+        'square_foot': 0.3048**2,
+        'square_yard': 0.9144**2
+    }
 
-    # Angle
-    _DEG_TO_RAD = 0.0174533
+    # Volume (base unit: cubic meter)
+    # Volume conversions are derived from length conversions cubed where applicable.
+    volume_conversions = {
+        'cubic_meter': 1.0,
+        'liter': 0.001,
+        'deciliter': 0.001/10,
+        'centiliter': 0.001/100,
+        'milliliter': 0.001/1000,
+        'cubic_inch': (0.0254**3),
+        'cubic_foot': (0.3048**3),
+        'cubic_yard': (0.9144**3),
+        'gallon': 0.00378541,
+        'quart': 0.00378541/4,
+        'pint': 0.00378541/8,
+        'fluid_ounce': 0.00378541/128,
+        'bushel': 0.0352391,
+        'barrel': 0.158987
+    }
 
-    def __init__(self, factor):
+    # Power (base unit: watt)
+    power_conversions = {
+        'watt': 1.0,
+        'kilowatt': 1000.0,
+        'btu_per_hour': 0.29307107,
+        'horsepower': 745.7
+    }
+
+    # Pressure (base unit: pascal)
+    # psi and psf use lbf (pound-force) and inch/foot conversions:
+    # lbf to newton: 1 lbf = 4.44822 N
+    # psi = lbf/in² = 4.44822 / (0.0254²) Pa
+    # psf = lbf/ft²  = 4.44822 / (0.3048²) Pa
+    _LBF_TO_N = 4.44822
+    _IN2 = (0.0254**2)
+    _FT2 = (0.3048**2)
+
+    pressure_conversions = {
+        'pascal': 1.0,
+        'atmosphere': 101325.0,
+        'bar': 100000.0,
+        'millibar': 100.0,
+        'megapascal': 1e6,
+        'kilopascal': 1000.0,
+        'psi': _LBF_TO_N / _IN2,
+        'psf': _LBF_TO_N / _FT2
+    }
+
+    # Speed (base unit: meter per second)
+    # knot = nautical mile / hour
+    # mph = mile / hour
+    # fps = foot / second
+    # fpm = foot / minute
+    knot_to_ms = length_conversions['nautical_mile'] / time_conversions['hour']
+    mph_to_ms = length_conversions['mile'] / time_conversions['hour']
+    fps_to_ms = length_conversions['foot']
+    fpm_to_ms = length_conversions['foot'] / time_conversions['minute']
+    kms_to_ms = length_conversions['km']  # km/s (just scale length)
+
+    speed_conversions = {
+        'meter_per_second': 1.0,
+        'kilometer_per_second': kms_to_ms,
+        'knots': knot_to_ms,
+        'mile_per_hour': mph_to_ms,
+        'feet_per_second': fps_to_ms,
+        'feet_per_minute': fpm_to_ms
+    }
+
+    # Force (base unit: newton)
+    # We provide lbf as a convenience.
+    force_conversions = {
+        'newton': 1.0,
+        'pound_force': _LBF_TO_N
+    }
+
+    # The following method transforms a dictionary of unit factors into class properties.
+    @classmethod
+    def _create_properties(cls, conversions_dict):
+        for unit_name, factor in conversions_dict.items():
+            # Create a property for each unit
+            # Example: @property def mm(cls): return cls(factor)
+            # Instead of rewriting, we can use a closure:
+            def make_property(f):
+                return property(lambda self: self.__class__(f))
+            
+            # For class-level access, we define it as a classmethod property
+            # We’ll define a small descriptor that returns a Units instance:
+            setattr(cls, unit_name, classmethod(property(lambda c, f=factor: c(f))))
+
+    def __init__(self, factor: float):
         self.factor = factor
 
     def __mul__(self, value):
+        # Convert value to base unit
         return value * self.factor
 
     def __rmul__(self, value):
+        # Convert value to base unit (right multiplication)
         return value * self.factor
 
     def __truediv__(self, value):
+        # Convert from base unit to target unit
         return value / self.factor
 
-    # Area properties
-    @classmethod
-    @property
-    def acre(cls): return cls(cls._ACRE_TO_SQM)
-    
-    @classmethod
-    @property
-    def square_yard(cls): return cls(cls._SQYARD_TO_SQM)
-    
-    @classmethod
-    @property
-    def square_feet(cls): return cls(cls._SQFEET_TO_SQM)
-    
-    @classmethod
-    @property
-    def square_inch(cls): return cls(cls._SQINCH_TO_SQM)
-    
-    @classmethod
-    @property
-    def square_kilometer(cls): return cls(cls._SQKM_TO_SQM)
-    
-    @classmethod
-    @property
-    def square_decimeter(cls): return cls(cls._SQDM_TO_SQM)
-    
-    @classmethod
-    @property
-    def square_meter(cls): return cls(1.0)
-    
-    @classmethod
-    @property
-    def square_centimeter(cls): return cls(cls._SQCM_TO_SQM)
-    
-    @classmethod
-    @property
-    def hectare(cls): return cls(cls._HECTARE_TO_SQM)
-
-    # Power properties
-    @classmethod
-    @property
-    def kilowatt(cls): return cls(cls._KW_TO_W)
-    
-    @classmethod
-    @property
-    def btu_per_hour(cls): return cls(cls._BTU_PER_HOUR_TO_W)
-    
-    @classmethod
-    @property
-    def horsepower(cls): return cls(cls._HP_TO_W)
-    
-    @classmethod
-    @property
-    def watt(cls): return cls(1.0)
-
-    # Pressure properties
-    @classmethod
-    @property
-    def atmosphere(cls): return cls(cls._ATM_TO_PA)
-    
-    @classmethod
-    @property
-    def bar(cls): return cls(cls._BAR_TO_PA)
-    
-    @classmethod
-    @property
-    def millibar(cls): return cls(cls._MBAR_TO_PA)
-    
-    @classmethod
-    @property
-    def pascal(cls): return cls(1.0)
-    
-    @classmethod
-    @property
-    def megapascal(cls): return cls(cls._MPA_TO_PA)
-    
-    @classmethod
-    @property
-    def kilopascal(cls): return cls(cls._KPA_TO_PA)
-    
-    @classmethod
-    @property
-    def psf(cls): return cls(cls._PSF_TO_PA)
-    
-    @classmethod
-    @property
-    def psi(cls): return cls(cls._PSI_TO_PA)
-
-    # Speed properties
-    @classmethod
-    @property
-    def knots(cls): return cls(cls._KNOT_TO_MS)
-    
-    @classmethod
-    @property
-    def kilometer_per_second(cls): return cls(cls._KMS_TO_MS)
-    
-    @classmethod
-    @property
-    def meter_per_second(cls): return cls(1.0)
-    
-    @classmethod
-    @property
-    def mile_per_hour(cls): return cls(cls._MPH_TO_MS)
-    
-    @classmethod
-    @property
-    def feet_per_minute(cls): return cls(cls._FPM_TO_MS)
-    
-    @classmethod
-    @property
-    def feet_per_second(cls): return cls(cls._FPS_TO_MS)
-
-    # Time properties
-    @classmethod
-    @property
-    def minute(cls): return cls(cls._MIN_TO_S)
-    
-    @classmethod
-    @property
-    def hour(cls): return cls(cls._HOUR_TO_S)
-    
-    @classmethod
-    @property
-    def second(cls): return cls(1.0)
-    
-    @classmethod
-    @property
-    def day(cls): return cls(cls._DAY_TO_S)
-    
-    @classmethod
-    @property
-    def week(cls): return cls(cls._WEEK_TO_S)
-    
-    @classmethod
-    @property
-    def month(cls): return cls(cls._MONTH_TO_S)
-    
-    @classmethod
-    @property
-    def year(cls): return cls(cls._YEAR_TO_S)
-    
-    @classmethod
-    @property
-    def millisecond(cls): return cls(cls._MS_TO_S)
-
-    # Volume properties
-    @classmethod
-    @property
-    def cubic_meter(cls): return cls(1.0)
-    
-    @classmethod
-    @property
-    def liter(cls): return cls(cls._LITER_TO_M3)
-    
-    @classmethod
-    @property
-    def deciliter(cls): return cls(cls._DL_TO_M3)
-    
-    @classmethod
-    @property
-    def centiliter(cls): return cls(cls._CL_TO_M3)
-    
-    @classmethod
-    @property
-    def milliliter(cls): return cls(cls._ML_TO_M3)
-    
-    @classmethod
-    @property
-    def cubic_yard(cls): return cls(cls._CUYD_TO_M3)
-    
-    @classmethod
-    @property
-    def cubic_foot(cls): return cls(cls._CUFT_TO_M3)
-    
-    @classmethod
-    @property
-    def cubic_inch(cls): return cls(cls._CUIN_TO_M3)
-    
-    @classmethod
-    @property
-    def bushel(cls): return cls(cls._BUSHEL_TO_M3)
-    
-    @classmethod
-    @property
-    def barrel(cls): return cls(cls._BARREL_TO_M3)
-    
-    @classmethod
-    @property
-    def fluid_ounce(cls): return cls(cls._FLOZ_TO_M3)
-    
-    @classmethod
-    @property
-    def gallon(cls): return cls(cls._GALLON_TO_M3)
-    
-    @classmethod
-    @property
-    def pint(cls): return cls(cls._PINT_TO_M3)
-    
-    @classmethod
-    @property
-    def quart(cls): return cls(cls._QUART_TO_M3)
-
-    # Mass properties
-    @classmethod
-    @property
-    def kilogram(cls): return cls(1.0)
-    
-    @classmethod
-    @property
-    def gram(cls): return cls(cls._GRAM_TO_KG)
-    
-    @classmethod
-    @property
-    def milligram(cls): return cls(cls._MG_TO_KG)
-    
-    @classmethod
-    @property
-    def pound(cls): return cls(cls._POUND_TO_KG)
-    
-    @classmethod
-    @property
-    def lbs(cls): return cls(cls._POUND_TO_KG)
-    
-    @classmethod
-    @property
-    def lb(cls): return cls(cls._POUND_TO_KG)
-    
-    @classmethod
-    @property
-    def ounce(cls): return cls(cls._OUNCE_TO_KG)
-    
-    @classmethod
-    @property
-    def metric_ton(cls): return cls(cls._TON_TO_KG)
-
-    # Angle properties
-    @classmethod
-    @property
-    def degree(cls): return cls(cls._DEG_TO_RAD)
-    
-    @classmethod
-    @property
-    def radian(cls): return cls(1.0)
+# Dynamically create the unit properties
+Units._create_properties(Units.length_conversions)
+Units._create_properties(Units.time_conversions)
+Units._create_properties(Units.mass_conversions)
+Units._create_properties(Units.angle_conversions)
+Units._create_properties(Units.current_conversions)
+Units._create_properties(Units.luminous_intensity_conversions)
+Units._create_properties(Units.temperature_conversions)
+Units._create_properties(Units.area_conversions)
+Units._create_properties(Units.volume_conversions)
+Units._create_properties(Units.power_conversions)
+Units._create_properties(Units.pressure_conversions)
+Units._create_properties(Units.speed_conversions)
+Units._create_properties(Units.force_conversions)
