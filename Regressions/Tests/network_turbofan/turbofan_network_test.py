184df03c57d5a1baf3ca0d6a969ec3822ed5b2d1
Matthew Clarke <mclarke2@stanford.edu>
2024-12-29 13:46:57 -0800

    updates

--- Regressions/Tests/network_turbofan/turbofan_network_test.py
+++ Regressions/Tests/network_turbofan/turbofan_network_test.py
@@ -100,31 +100,31 @@ def main():
             print(val)
     
     # Truth values
-    takeoff_thrust_truth     = 99134.27996755976
-    climb_throttle_1_truth   = 1.3958540053646624
-    climb_throttle_2_truth   = 1.0684497367073507
+    takeoff_thrust_truth     = 99134.30160723036
+    climb_throttle_1_truth   = 1.39585770099276
+    climb_throttle_2_truth   = 1.068449685027111
     climb_throttle_3_truth   = 0.5
-    climb_throttle_4_truth   = 0.8680810572703119
-    climb_throttle_5_truth   = 0.8753486973284144
-    climb_throttle_6_truth   = 1.166851915942572
-    climb_throttle_7_truth   = 1.3116873483854699
-    climb_throttle_8_truth   = 0.6075724027269327
-    climb_throttle_9_truth   = 0.8790353464741087
-    climb_10_CL_truth        = 1.387293256534933
-    cruise_CL_1_truth        = 0.6738526732935098
-    cruise_CL_2_truth        = 0.6686241746393166
-    cruise_CL_3_truth        = 0.45025703356067937
-    descent_throttle_1_truth = 0.11727849456204754
-    curved_cruise_CL_truth   = 1.2889478757967596
+    climb_throttle_4_truth   = 0.8680810461836567
+    climb_throttle_5_truth   = 0.8753486891156845
+    climb_throttle_6_truth   = 1.166851904716598
+    climb_throttle_7_truth   = 1.311687335353929
+    climb_throttle_8_truth   = 0.607572398006494
+    climb_throttle_9_truth   = 0.8790351465932528
+    climb_10_CL_truth        = 1.38729324597682
+    cruise_CL_1_truth        = 0.6738526681840649
+    cruise_CL_2_truth        = 0.6686241695533797
+    cruise_CL_3_truth        = 0.45025703224293206
+    descent_throttle_1_truth = 0.11727849354251198
+    curved_cruise_CL_truth   = 1.2889478664248029
     descent_throttle_2_truth = 0.5
-    single_pt_CL_1_truth     = 0.0005711272589290616
-    single_pt_CL_2_truth     = 0.000661628495326631
-    loiter_1_CL_truth        = 0.47987168897708904
-    loiter_2_CL_truth        = 0.47986558694994014
-    reserve_1_CL_truth       = 0.32386480839522064
-    reserve_2_CL_truth       = 0.3155985800619528
-    descent_throttle_3_truth = 0.15179606805705192
-    landing_thrust_truth     = 10914.920312664697
+    single_pt_CL_1_truth     = 0.0005711272589290973
+    single_pt_CL_2_truth     = 0.0006616284953266113
+    loiter_1_CL_truth        = 0.47987168527259755
+    loiter_2_CL_truth        = 0.479865583245496
+    reserve_1_CL_truth       = 0.323864805860335
+    reserve_2_CL_truth       = 0.3155985775560958
+    descent_throttle_3_truth = 0.1517960657118641
+    landing_thrust_truth     = 10914.922642740088
     
     # Store errors 
     error = Data()
