d0c64720aeedd2e2bf78b2dcd198a44e0a36050f
Matthew Clarke <mclarke2@stanford.edu>
2024-12-29 17:03:37 -0800

    Update cg_and_moi_test.py

--- Regressions/Tests/analysis_weights/cg_and_moi_test.py
+++ Regressions/Tests/analysis_weights/cg_and_moi_test.py
@@ -152,9 +152,9 @@ def EVTOL_Aircraft_Test(update_regression_values):
     
     print(weight_analysis.vehicle.tag + ' Moment of Intertia')
     print(MOI) 
-    accepted  = np.array([[ 6158.27730029,  -599.57162407,  -726.88178703],
-                          [ -599.57162407,  9527.43492578,   -96.20103752],
-                          [ -726.88178703,   -96.20103752, 14304.59473911]])
+    accepted  = np.array([[6165.3741424348045,-599.5553934443328, -727.0671702223518],
+                          [-599.5553934443328, 9530.162546879752,  -96.17454446468206],
+                          [ -727.0671702223518,  -96.17454446468206, 14313.643487247744]])
     MOI_error     = MOI - accepted
 
     # Check the errors
